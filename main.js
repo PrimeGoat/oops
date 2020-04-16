930b41a8042a9d717252a08a336ad72dd8057ca8
DENIS SAVGIR <denis@DENISs-MBP.home>
2020-04-16 10:44:16 -0400

    Done

--- main.js
+++ main.js
@@ -1,7 +1,37 @@
 const Stack = function() {
+  return {
+    items: [],
+
+    add: function(item) {
+      this.items.push(item);
+    },
+
+    remove: function() {
+      return this.items.pop();
+    },
+
+    peek: function() {
+      return this.items[this.items.length - 1];
+    }
+  }
 }
 
 const Queue = function() {
+  return {
+    items: [],
+
+    add: function(item) {
+      this.items.push(item);
+    },
+
+    remove: function() {
+      return this.items.shift();
+    },
+
+    peek: function() {
+      return this.items[0];
+    }
+  }
 }
 
 module.exports = {
